f76dd7880b095e86fdf882c0ef2a7340b1317e93
heyjulieh <heyjulieh@gmail.com>
2017-03-29 00:36:13 -0700

    updated security_questions.js

--- starter-code/security_questions.js
+++ starter-code/security_questions.js
@@ -1,1 +1,31 @@
 console.log("security_questions.js is loaded");
+//security questions
+var securityQuestions = [
+  {
+    question: "What is your mother's maiden name?",
+    expectedAnswer: "Chou",
+  },
+  {
+    question: "What is your favorite color?",
+    expectedAnswer: "Red",
+  },
+  {
+    question: "What is your favorite city?",
+    expectedAnswer: "Los Angeles",
+  }
+];
+  //loop prompt
+  var userResponse = "";
+  var userIncorrect = false;
+
+  for (i=0; i<securityQuestions.length; i++){
+    userResponse = prompt(securityQuestions[i].question);
+    if (userResponse !== securityQuestions[i].expectedAnswer){
+      alert("Your answer was incorrect!");
+      userIncorrect = true;
+      break;
+    }
+    }
+    if (userIncorrect){
+      alert("You do not have any more password attempts.");
+    }
